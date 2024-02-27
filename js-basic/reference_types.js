b351856948e430ff9e1b9c7bf2263e81c9448025
G Dev3 <gdev3.js@gmail.com>
2024-02-28 01:52:12 +0530

    Arrays intro

--- js-basic/reference_types.js
+++ js-basic/reference_types.js
@@ -26,6 +26,18 @@ person['name'] ='G Developer'
 
 console.log(person.name)
 
+// Arrays
+
+// Array is an objects
+let selectedColors = ['red','blue']; 
+
+console.log(selectedColors);
+
+console.log(selectedColors[0]);
 
 
+selectedColors[2] = 1;
 
+console.log(selectedColors);
+
+console.log(selectedColors.length);
