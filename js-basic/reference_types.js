b01f19a13a866ace9c745a39bee5998186989e04
G Dev3 <gdev3.js@gmail.com>
2024-02-28 01:46:40 +0530

    reference type objects

---
+++ js-basic/reference_types.js
@@ -0,0 +1,31 @@
+// reference types data types
+
+// Object
+// Arrays 
+// function
+
+
+// Object
+let person = {
+  name:'Gopal',
+  age:21,
+  isloggedin: true
+}
+
+console.log(person.name)
+
+console.log(person.age)
+
+person.name = 'Mosh'
+
+//Dot notation
+console.log(person.name)
+
+// Bracket Notation
+person['name'] ='G Developer'
+
+console.log(person.name)
+
+
+
+
