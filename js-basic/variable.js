748c828bb56a0127e3d93b34b4234f49c419b543
G Dev3 <gdev3.js@gmail.com>
2024-02-28 01:20:51 +0530

    Variable

---
+++ js-basic/variable.js
@@ -0,0 +1,18 @@
+let name = 'Mosh';
+
+console.log(name)
+
+// cannot be a reserved keyword
+
+// They should be meaningful name
+
+// Cannot start with a number {1name}
+
+// Cannot contain a space or hypen (-)
+
+// They are case sensitive
+
+// Always use semi-colon in the end of the element
+
+let firstName, lastname;  // do not declare like this, it's a bad habit
+
