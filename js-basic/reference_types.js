6013235f67a281d840e7c03146d98244ddecff96
G Dev3 <gdev3.js@gmail.com>
2024-02-28 02:00:28 +0530

    Function intro

--- js-basic/reference_types.js
+++ js-basic/reference_types.js
@@ -1,43 +1,59 @@
 // reference types data types
 
 // Object
-// Arrays 
+// Arrays
 // function
-
 
 // Object
 let person = {
-  name:'Gopal',
-  age:21,
-  isloggedin: true
-}
+  name: "Gopal",
+  age: 21,
+  isloggedin: true,
+};
 
-console.log(person.name)
+console.log(person.name);
 
-console.log(person.age)
+console.log(person.age);
 
-person.name = 'Mosh'
+person.name = "Mosh";
 
 //Dot notation
-console.log(person.name)
+console.log(person.name);
 
 // Bracket Notation
-person['name'] ='G Developer'
+person["name"] = "G Developer";
 
-console.log(person.name)
+console.log(person.name);
 
 // Arrays
 
 // Array is an objects
-let selectedColors = ['red','blue']; 
+let selectedColors = ["red", "blue"];
 
 console.log(selectedColors);
 
 console.log(selectedColors[0]);
-
 
 selectedColors[2] = 1;
 
 console.log(selectedColors);
 
 console.log(selectedColors.length);
+
+// Functions
+
+function greet() {
+  console.log("hello World");
+}
+
+greet();
+
+function responce(name) {
+  console.log("hello " + name);
+}
+
+responce("Gopal",'Dev');
+responce("Sunil");
+responce("G dev3");
+
+
