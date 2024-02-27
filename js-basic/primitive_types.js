3c335f8415f5b16a854b42a345c697132d4b57b1
G Dev3 <gdev3.js@gmail.com>
2024-02-28 01:39:09 +0530

    primitive data types

---
+++ js-basic/primitive_types.js
@@ -0,0 +1,25 @@
+// Primitive data type
+
+let name = 'Mosh'; // String literal
+
+let age = 30;   // Number literal
+
+let isApproved = true; // Boolean literal
+
+let firstName; // undefined literal
+
+let lastName = null; // null literal
+
+// Javascript is a Dynamic type language
+
+let personName = 'harry'
+
+console.log(typeof(personName))
+// String
+personName = 23;
+
+console.log(typeof(personName))
+// number 
+
+
+// with help of the personName example we confirmed that javascript it dynamic type language 
