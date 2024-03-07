8922e2f4cfbaaa26522e31acc40149888fd83544
G Dev3 <gdev3.js@gmail.com>
2024-03-07 16:37:34 +0530

    Oprators

---
+++ js-basic/oprators.js
@@ -0,0 +1,15 @@
+let x = 10;
+let y = (x>5)&&(x<15);
+// Answer is true
+
+let a = 5;
+a += 3;
+// Answer is 8
+
+let b = 10;
+let c = b++
+// Answer is 10
+
+let d = 1;
+let e = d !== 2;
+// Answer is true
